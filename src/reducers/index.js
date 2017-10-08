de3d65b71b20c520ed44f5b7b9c9eec4adf27ad0
yonggoonoh <nohyg90@gmail.com>
2017-10-08 11:52:06 +0900

    create reducers and combine them

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+import counter from './counter';
+import ui from './ui';
+
+const reducers = combineReducers({
+    counter,
+    ui,
+});
+
+export default reducers
