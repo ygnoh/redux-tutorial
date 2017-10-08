5f14aae7396a4ce7a3ab7b6c526f23517cad6b2a
yonggoonoh <nohyg90@gmail.com>
2017-10-08 14:41:28 +0900

    onRandomizeColor 구현

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,13 +7,35 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.setRandomColor = this.setRandomColor.bind(this);
+    }
+
+    setRandomColor() {
+        const color = [
+            Math.floor((Math.random()*55) + 200),
+            Math.floor((Math.random()*55) + 200),
+            Math.floor((Math.random()*55) + 200)
+        ];
+
+        this.props.handleSetColor(color);
+    }
+
     render () {
+        const color = this.props.color;
+
+        const style = {
+            background: `rgb(${color[0]}, ${color[1]}, ${color[2]})`,
+        };
+
         return (
-            <div>
+            <div style={style}>
                 <Value number={this.props.number} />
                 <Control
                     onPlus={this.props.handleIncrement}
                     onSubtract={this.props.handleDecrement}
+                    onRandomizeColor={this.setRandomColor}
                 />
             </div>
         )
