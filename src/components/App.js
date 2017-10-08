9669a05aa4f0b66227592dd46a2aef00bd2e1f1c
yonggoonoh <nohyg90@gmail.com>
2017-10-08 11:22:15 +0900

    Create and Render Counter.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import Counter from './Counter';
+
 class App extends Component {
     render () {
         return (
             <div>
-                App
+				<Counter />
             </div>
         )
     }
