39df617eaf2a7698c4592aae1d3462713fb68e96
yonggoonoh <nohyg90@gmail.com>
2017-10-08 14:32:58 +0900

    각 component와 redux 연결하여 props로 state전달

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,15 +3,37 @@ import React, { Component } from 'react'
 import Value from './Value';
 import Control from './Control';
 
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 class Counter extends Component {
     render () {
         return (
             <div>
-                <Value />
-                <Control />
+                <Value number={this.props.number} />
+                <Control
+                    onPlus={this.props.handleIncrement}
+                    onSubtract={this.props.handleDecrement}
+                />
             </div>
         )
     }
 }
 
-export default Counter
+const mapStateToProps = (state) => {
+    return {
+        number: state.counter.number,
+        color: state.ui.color,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        handleIncrement: () => {dispatch(actions.increment())},
+        handleDecrement: () => {dispatch(actions.decrement())},
+        handleSetColor: (color) => {dispatch(actions.setColor(color))},
+
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
