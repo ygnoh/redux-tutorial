cae5fd99db4b7b6e39cacdceddaee05aad40d4dc
yonggoonoh <nohyg90@gmail.com>
2017-10-08 12:10:34 +0900

    store test

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+import * as actions from './actions';
+
+const store = createStore(reducers);
+
+console.log(store.getState());
+const unsubscribe = store.subscribe(() => console.log(store.getState()));
+store.dispatch(actions.increment());
+store.dispatch(actions.increment());
+store.dispatch(actions.decrement());
+store.dispatch(actions.setColor([200, 200, 200]));
+
+unsubscribe();
+
+store.dispatch(actions.setColor([210, 210, 210]));
+
+
 ReactDOM.render(
     <App />,
     document.getElementById("root")
